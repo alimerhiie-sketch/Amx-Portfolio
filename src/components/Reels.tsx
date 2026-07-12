c149c677957baac77af4f7d04ddf8803438b77d4
alimerhiie-sketch <ali.merhiie@gmail.com>
2026-07-12 21:21:03 +0300

    fix storj raw url for video

--- src/components/Reels.tsx
+++ src/components/Reels.tsx
@@ -48,7 +48,7 @@ const ALL_REELS = [
 ];
 
 const ALL_CATEGORIES = ["All", ...Array.from(new Set(ALL_REELS.map((r) => r.category)))];
-const STORJ_BASE = "https://link.storjshare.io/s/jwgdhfs6it4vyrx45oujruwxco6a/amxreels";
+const STORJ_BASE = "https://link.storjshare.io/raw/jwgdhfs6it4vyrx45oujruwxco6a/amxreels";
 
 function ReelCard({
   reel,
